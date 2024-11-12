bb2acde7edfd301af044a95c8d1f5eb13dfc4ddb
moonhr <moonhr153@gmail.com>
2024-11-12 14:24:03 +0900

    fix | item이 배열을 받고 list가 item에 배열을 전달하도록 수정

--- src/checked/checkedItem.jsx
+++ src/checked/checkedItem.jsx
@@ -1,18 +1,48 @@
-import React, {useState} from "react";
-const CheckedItem = ({item}) => {
+import React, {useEffect, useState} from "react";
+const CheckedItem = (item, onChangeCheck) => {
 
   const [isChecked, setIsChecked]= useState(true)
 
   const handleInput = () => {
     setIsChecked(false)
+    // item.checked = false
   }
 
   return(
-  <div>
+  <div id={item.id}>
     <input type="checkbox" checked={isChecked} onChange={handleInput}/>
     <p>{item.title}</p>
     <p>{item.update_at}</p>
   </div>);
 }
 
-export default CheckedItem;
+// export default CheckedItem;
+
+const CheckedList = (data, changeData) => {
+
+  const handleButton = () => {
+
+  }
+
+  const toggleCheckbox = (id) => {
+    const rest = data.filter(e => e.id !== id);
+    const target = data.fild(e => e.id === id);
+    target.checked = !target.checked;
+
+    const newArr = [...rest, target];
+
+    changeData(newArr)
+  }
+
+  return(
+    <>
+    {data.filter(item => item.checked ===true).map(item=> {
+      return(<CheckedItem item={item} onChangeCheck={toggleCheckbox}/>)
+      })
+    }
+      <button onClick={handleButton} />
+    </>
+    );
+}
+
+export default CheckedList
